01fc4ffb658874f913a13f228e321d741d2e9851
Bodomit <gbrown29@qub.ac.uk>
2020-01-14 13:12:09 +0000

    Get all voices for all audio.

--- text_to_speech.py
+++ text_to_speech.py
@@ -19,30 +19,30 @@ voices = [
 def text_to_speech(text: str, path: str): 
     synthesis_input = texttospeech.types.SynthesisInput(text=text)
 
-    voice = texttospeech.types.VoiceSelectionParams(
-        name=random.choice(voices),
-        language_code='ja-JP')
-    audio_config = texttospeech.types.AudioConfig(
-        audio_encoding=texttospeech.enums.AudioEncoding.MP3)
+    for i, voice_name in enumerate(voices):
+        voice = texttospeech.types.VoiceSelectionParams(
+            name=voice_name,
+            language_code='ja-JP')
+        audio_config = texttospeech.types.AudioConfig(
+            audio_encoding=texttospeech.enums.AudioEncoding.MP3)
 
-    response = client.synthesize_speech(synthesis_input, voice, audio_config)
+        response = client.synthesize_speech(synthesis_input, voice, audio_config)
 
-    with open(path, 'wb') as out:
-        out.write(response.audio_content)
+        with open(path + "_v{}.mp3".format(i), 'wb') as out:
+            out.write(response.audio_content)
 
 def main():
     entries = load_data()
-
-    base_path = os.path.join("resources", "audio")
+    base_path = os.path.join("C:\\", "Users", "40057686", "OneDrive", "JPN5000_Audio2")
     os.makedirs(base_path)
 
     for entry in entries:
         id = int(entry["data"]["entry_id"])
-        audio_filename = "{0:04d}_entry.mp3".format(id)
+        audio_filename = "{0:04d}_entry".format(id)
         text_to_speech(entry["data"]["entry_name"], os.path.join(base_path, audio_filename))
 
         for i, example in enumerate(entry["examples"]):
-            audio_filename = "{0:04d}_example_{1}.mp3".format(id, i)
+            audio_filename = "{0:04d}_example_{1}".format(id, i)
             text_to_speech(example["jp"], os.path.join(base_path, audio_filename))
     print("Complete!")
 
